9612c0af031ef3ba87c14b32cb96e9555b9bcfa4
Aditi Mishra <adimishr@192.168.0.104>
2020-10-10 12:29:16 +0530

    removed unnecessary files

--- Scrapping/qeVSstage.py
+++
@@ -1,8 +0,0 @@
-
-fqe=open("pluginUpdatesQE.csv","r")
-fstage=open("pluginUpdatesStage.csv","r")
-fqs=open("pluginQEvsStage.csv","w")
-for line1,line2 in zip(fqe,fstage):
-    str = line1.strip()+","+line2.strip()
-    fqs.write(str)
-    fqs.write("\n")
